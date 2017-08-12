679e0fc0a9291cd0cfd5446c4e197d0d3a2c3dec
mathiew <mathiew@fourier.berkeley.edu>
2017-08-12 15:40:53 -0700

    Added the changes for email worked out the org-mode problem reloading the old files in init.el Added Reveal.js

--- init.el
+++ init.el
@@ -1,1 +1,12 @@
+(package-initialize)
+(require 'cl)
+
+  ;; Org-mode that was shipped with Emacs
+  (setq load-path (remove-if (lambda (x) (string-match-p "org$" x)) load-path))
+  ;; ELPA 
+  (setq load-path (remove-if (lambda (x) (string-match-p "org-20" x)) load-path))
+
+  (setq custom-org-path "/home/mathiew/.emacs.d/.cask/26.0/elpa/org-20170807")   
+  (add-to-list 'load-path custom-org-path)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
